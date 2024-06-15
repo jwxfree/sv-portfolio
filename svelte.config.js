cc5085abb456714d4d3d52457ae3abc7d11e34d3
jwxfree <aeronescalante@gmail.com>
2024-06-15 22:30:36 +0800

    fixed svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,8 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      // Add Vercel adapter options here if needed
-    })
+    adapter: adapter(),
+    target: '#svelte',
   },
   preprocess: vitePreprocess()
 };
